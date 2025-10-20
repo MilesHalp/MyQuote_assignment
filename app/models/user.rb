87362ace912a8f93670eabeefbfb47621a942bdc
MilesHalp <mileshalp@gmail.com>
2025-10-20 12:40:14 +0000

    Additional quotes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    has_secure_password #Handles validating the password
+    has_secure_password #Handles validating that the password must be filled. Should find way to ensure uniqueness against hashed passwords
     has_many :quotes, dependent: :destroy
 
-    # Validations that require the first name, last name and email to be filled
+    # Validations that require the first name, last name and email to be filled, with emails being unique
     validates :fname, presence: true
     validates :lname, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: true
+
 end
